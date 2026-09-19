7522dae322d92f1efc894664bf97a98416749671
Riya Ahuja <riya.m.ahuja182@gcoea.ac.in>
2026-09-19 15:32:09 +0530

    Time: O(N log N), Space: O(N)

---
+++ 708874-overlapping-intervals-GFG/708874-overlapping-intervals.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    vector<vector<int>> mergeOverlap(vector<vector<int>>& intervals) {
+        // Code here
+        if(intervals.empty()) return {};
+        
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+    
+        ans.push_back(intervals[0]);
+    
+        for(int i = 1;i < intervals.size();i++){
+    
+            if(intervals[i][0] <= ans.back()[1]){
+    
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+            else {
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
